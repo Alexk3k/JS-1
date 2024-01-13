b96a33061b2d44dc76c762696332f37f9971c645
Alexk3k <valderrama06@gmail.com>
2024-01-13 23:06:24 +0300

    2nd task

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 /*
-Ваша часовая ставка 80$ и вы готовы работать не
-более 5 часов в день 5 дней в недлю (кроме выходных).
-К вам приходит заказчик и предлагает заказ на 40 
-часов работы.
-Сейчас понедельникю
-Вы должны уехать через 11 дней
-Выведете в консоль:
-- Boolean переменную успеете ли вы взяться за работу
-- Сколько вы за неё попросите?
+    Вася положил 12 000$ на вклад 7% годовых с
+    капитализацией 1 раз в месяц.
+    Вывести в консоль, сможет ли нон купить дом за 13 500$
+    через 2 года после снятия вклада. И остаток после покупки.
+
+    Итог = Сумма * (1 + Ставка в месяц не в %) ^ срок в месяцах
 */ 
 
-const hourPayment = 80;
-const workHours = 5;
-const workDays = 5;
-const jobHours = 40;
-const workDaysWOWeekend = 9;
-const allDays = 11;
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const houseCost = 13500;
 
-let payment = hourPayment * jobHours;
-console.log(payment + "$");
-console.log("Can i work?" + " " + (workDaysWOWeekend < allDays)); 
+let money = (deposit * (1+ rate / 12) ** depositLength)
+console.log(money);
+
+if (money > houseCost){
+    console.log(`U have ${money} and Yes u can buy a house, gratz. 
+Balance ${money - houseCost } ` )
+} else{
+    console.log(`U have ${money} not enought money :( `)
+}
