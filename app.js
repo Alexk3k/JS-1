94d2e6f4839eff597fcbdeee7857abe4fd80aa6d
Alexk3k <valderrama06@gmail.com>
2024-01-31 21:13:44 +0300

    9th task

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 /*
-    Дан произвольный url -
-    'https://purpleschool.ru/course/javascript'
-    Нужно сделать функцию, которая выводит в консоль:
-    - Протокол (https)
-    - Доменное имя (purpleschool.ru)
-    - Путь внутри сайта (/course/javascript)
+    Задача вывести в консоль строку "Я люблю JS !" из массива,
+    прозодя циклом в обратном порядке, не используя метод reverse.
+    const arr = ['!', 'JS', 'люблю', 'Я'];
 */
 
-const url = 'https://purpleschool.ru/course/javascript';
-const url2 = 'https://docs.google.com/spreadsheets/d/';
+const arr = ['!', 'JS', 'люблю', 'Я'];
+const newArr = [];
 
+for(let i = (arr.length - 1); i >= 0; i --){
+    newArr.push(arr[i]);
+};
 
-function urlSplit(url){
-    const [protocol, _, domain, ...path] = url.split('/')
-    console.log(protocol, _, domain, path)
-    console.log(`Протокол: ${protocol.split(':')[0]}`);
-    console.log(`Доменное имя: ${domain}`);
-    console.log(`Путь внутри сайта: ${path.join('/')}`);
-    // const res = url.split('/');
-    // const protocol = res[0];
-    // const domain = res[2];
-    // const path = res[3] + ('/') + res[4];
-    // console.log('Протокол -', protocol);
-    // console.log('Доменное имя -', domain);
-    // console.log('Путь внутри сайта -', path);
-}
-
-urlSplit(url);
-urlSplit(url2);
+console.log(newArr.join(' '))
