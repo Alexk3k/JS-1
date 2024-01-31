0e6421d7416ccbcbccf508583e023769a8252608
Alexk3k <valderrama06@gmail.com>
2024-01-31 18:30:26 +0300

    7th task D.R.Y. method

--- app.js
+++ app.js
@@ -6,40 +6,38 @@
     - Удаление задачи по назвапнию
     - Перенос задачи в начало списка по названию
     Всегда меняем исходных массив
+
 */
 
 const tasks = ['Task 1'];
 
 function addTask(taskName) {
-    tasks.push(taskName) 
+    tasks.push(taskName); 
 };
-addTask('Task 2');
-addTask('Task 3');
-addTask('Task 4');
-
-console.log(tasks);
 
 function removeTask(taskName) {
    const numTask = tasks.indexOf(taskName);
    if (numTask === -1){
     return;
    }
-    tasks.splice(numTask, 1);
+    return tasks.splice(numTask, 1);
 };
-removeTask('Task 1');
-console.log(tasks)
 
 function replaceToStartTask(taskName) {
-    const numTask = tasks.indexOf(taskName);
-    if (numTask === -1){
+    const result = removeTask(taskName);
+    if (!result) {
         return;
     }
-    const oldTask = tasks[numTask];
-    tasks.splice(numTask, 1);
-    tasks.unshift(oldTask); 
-    };
+    tasks.unshift(result[0]);
+};
+
+addTask('Task 2');
+addTask('Task 3');
+addTask('Task 4');
+console.log(tasks);
+
+removeTask('Task 1');
+console.log(tasks);
 
 replaceToStartTask('Task 3')
-
-
 console.log(tasks)
