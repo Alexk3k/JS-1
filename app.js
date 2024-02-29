88fbf553c10c209e6f6fc0da33538e692dd2e94b
Alexk3k <valderrama06@gmail.com>
2024-02-29 18:21:08 +0300

    11th taks

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
 /*
-    Есть выгрузка операций пользователя
-    const operations = [1000, -700, 300, -500, -10000];
-    а так же начальный баланс в 100$
-    Необходимо сделать функции расчета:
-    - Итогового баланса
-    - Наличия отрицательного баланса (если после очередной операции
-            баланс < 0, то выдавать False)
-    - Расчета среднего расхода и среднего дохода
+    Имеется массив изменения цен prices, где внутри
+    1й элемент массива является ценой в момент Х,
+    2й - ценой в момент Y.
+    Нужно преобразовать данные в массив, где будут отображены
+    только положительные изменения цен: [100, 150]
 */
 
-const operations = [1000, -1700, 300, -500, 10000];
-const startBalance = 100;
+const prices = [[100, 200], [120, 100], [200, 350]];
 
-function showBalance(){
-    let balance = startBalance;
-    for (let i = 0; i < operations.length; i++) {
-        balance += operations[i];
-        if(balance < 0){
-        console.log(false);
-        }
-    }
-    console.log(balance)
-}
-
-function averageNum(){
-    let aboveZeroNum = 0;
-    let belowZeroNum = 0;
-    let plusCount = 0;
-    let minusCount = 0;
-
-    for (let i = 0; i < operations.length; i++) {
-        if (operations[i] > 0){
-            aboveZeroNum += operations[i];
-            plusCount ++;
-        }
-        
-        else if(operations[i] < 0){
-            belowZeroNum += operations[i];
-            minusCount ++;
-        }
-    }
-    console.log(aboveZeroNum / plusCount);
-    console.log(belowZeroNum / minusCount);
-}
-
-showBalance();
-averageNum();
+const positiveChanges = prices
+    .map(product => product[1] - product[0])
+    .filter(price => price > 0);
+console.log(positiveChanges);
