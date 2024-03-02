a868b50f4d48994949a63b22e0f7ddf538bdc777
Alexk3k <valderrama06@gmail.com>
2024-03-02 22:33:35 +0300

    Balance task #15

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-/* отсортировать пользователей по возрасту */
+/* Реальизовать методы увеличения и уменьшения баланса,
+    при котором каждая операция сохраняется в массив
+    opetarions в виде {reason : 'Оплата налогов', sum : -100}.
+    Возвращается true, если успешно и false, если не хватает баланса
+    Так же реализовать метод вволда числа операций по кошельку
+*/
 
-const users = [
-    { name : 'Вася', age : 30 },
-    { name : 'Катя', age : 18 },
-    { name : 'Аня', age : 40 },
-    { name : 'Петя', age : 25 },
-]
+const wallet = {
+    balance : 0,
+    increase : function(sum, reason){
+        this.balance += sum;
+        this.operations.push({
+            reason : reason,
+            sum : sum
+        })
+        return true;
+        
+},
+    decrease : function(sum, reason){
+        if(this.balance < sum){
+            console.log('Not enought money');
+            return false;
+        }
+        this.balance -= sum;
+        this.operations.push({
+            reason : reason,
+            sum : -sum
+        })
+        return true;
+},
+    getOperationLenght :function (){
+        return this.operations.length;
+    },
+    operations : []
+};
 
-console.log(users.sort((a, b) => a.age - b.age));
+console.log(wallet.increase(1000, 'ЗП'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(2000, 'Попука ПК'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.decrease(500, 'Покупка Телефона'));
+console.log(wallet.getOperationLenght());
+console.log(wallet.balance);
+console.log(wallet.operations);
+
