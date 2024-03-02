c3c1492851f959b7bd6146f52b4370d5a1ab8d0c
Alexk3k <valderrama06@gmail.com>
2024-03-02 18:43:39 +0300

    14th task

--- app.js
+++ app.js
@@ -1,49 +1,9 @@
-/* проверить является ли это номером телефона России */
+/* Замаскировать все, кроме последних 4х символов */
+/* ***********8353 */
 
-/* верные */
+const card = '2342834503458353';
 
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = '+7(910) 323-53-56   ';
-
-/* неверные */
-
-const num1Error = '89103235';
-const num2Error = '+7d910d323-53-56';
-const num3Error = '9+7103235356';
-const num4Error = '89103g235356';
-
-function isPhoneNumber(num){
-    num = num.trim();
-    num = num.replace ('+7', '8');
-    if(!num.startsWith(8)){
-        return false;
-    }
-    num = num.replaceAll ('(', '');
-    num = num.replaceAll (')', '');
-    num = num.replaceAll ('-', '');
-    num = num.replaceAll (' ', '');
-    if(num.length != 11){
-        return false;
-    }
-    let onlyNumber = true;
-    for (const char of num ){
-        if (isNaN(Number(char))){
-            onlyNumber = false;
-            break;
-        }
-    }
-    return onlyNumber;
-}
-
-console.log(isPhoneNumber(num1));
-console.log(isPhoneNumber(num2));
-console.log(isPhoneNumber(num3));
-console.log(isPhoneNumber(num4));
-console.log(isPhoneNumber(num5));
-console.log(isPhoneNumber(num1Error));
-console.log(isPhoneNumber(num2Error));
-console.log(isPhoneNumber(num3Error));
-console.log(isPhoneNumber(num4Error));
+const last4Nums = card.slice(-4);
+console.log(last4Nums);
+const markedCardNumber = last4Nums.padStart(card.length, '*');
+console.log(markedCardNumber);
