8e6f85e9734a5778645e6c6f420c09e5a0c030f4
Alexk3k <valderrama06@gmail.com>
2024-03-19 17:36:01 +0300

    19th task @Closures

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 /*
-    Создайте объект пользователя с паролем.
-    С помощью функции ниже удалить пароль сделав
-    функцию сброса пароля
+    Сделать функцию пользователя, которая берет за основу
+    userInfo и за счет замыкания создает новый объект, с
+    которым можно работать как user(1).increse(100)
 */
 
-function removePassword(reset){
-    if(reset){
-        this.password = undefined;
-    } else {
-        this.password = '1';
+const userInfo = {
+    balance : 0,
+    operations : 0,
+    increace(sum){
+        this.balance +=sum;
+        this.operations++;
     }
-};
-
-const user = {
-    login : 'Oleg@oleg.ru',
-    password : '123456'
 }
 
-const userResetPass = removePassword.bind(user, true);
+function user(){
+    const userObj = {
+    balance : 0,
+    operations : 0,
+    increace(sum){
+        this.balance +=sum;
+        this.operations++;
+    }
+}
+    return function(){
+        return userObj
+    }
+}
 
-console.log(userResetPass());
-console.log(user)
+const user1 = user()
+user1().increace(100);
+user1().increace(100);
+console.log(user1())
+
+const user2 = user();
+user2().increace(150);
+console.log(user2());
+
+// function changeBalance(){
+//     let balance = 0;
+//     return function(sum){
+//         balance += sum
+//         console.log(`Balance : ${balance}`)
+//     }
+// }
+
+// const change = changeBalance();
+
+// change(100);
+// change(-50);
+// change(200);
+
+// console.dir(change)
