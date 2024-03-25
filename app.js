290c7767a2e9789ddc7dc5279c164b3ebe1b48ae
Alexk3k <valderrama06@gmail.com>
2024-03-25 16:58:17 +0300

    DOM 20th

--- app.js
+++ app.js
@@ -1,53 +1,23 @@
-/*
-    Сделать функцию пользователя, которая берет за основу
-    userInfo и за счет замыкания создает новый объект, с
-    которым можно работать как user(1).increse(100)
-*/
+'use strict';
 
-const userInfo = {
-    balance : 0,
-    operations : 0,
-    increace(sum){
-        this.balance +=sum;
-        this.operations++;
-    }
-}
+document.querySelector('.input').value = 'Write a text';
 
-function user(){
-    const userObj = {
-    balance : 0,
-    operations : 0,
-    increace(sum){
-        this.balance +=sum;
-        this.operations++;
-    }
-}
-    return function(){
-        return userObj
-    }
-}
+// document.querySelector('.button').addEventListener('click', function(){
+//     const input = document.querySelector('.input').value;
+//     console.log(input);
+//     if(!input){
+//         return
+//     };
+//     document.querySelector('.panel').innerText = input;
+//     document.querySelector('.input').value = '';
+// })
 
-const user1 = user()
-user1().increace(100);
-user1().increace(100);
-console.log(user1())
-
-const user2 = user();
-user2().increace(150);
-console.log(user2());
-
-// function changeBalance(){
-//     let balance = 0;
-//     return function(sum){
-//         balance += sum
-//         console.log(`Balance : ${balance}`)
-//     }
-// }
-
-// const change = changeBalance();
-
-// change(100);
-// change(-50);
-// change(200);
-
-// console.dir(change)
+function changeClick() {
+    const input = document.querySelector('.input').value;
+    console.log(input);
+    if(!input){
+        return
+    };
+    document.querySelector('.panel').innerText = input;
+    document.querySelector('.input').value = '';
+};
